895b707348ab28c8bd2177833ffa9b6d523e57a5
Kelly Boettcher <kboettcher.kk@gmail.com>
2024-01-17 18:33:39 -0600

    Yuck

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+ var employee = {} ;
+
+function updateEmployeesWithKeyAndValue(employee, key,value) {
+    const employees = employee
+    return employees.streetAddress =  "11 Broadway"
+}
+    
+    
+    
+ 
+ 
+
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.streetAddress= "11 Broadway";
+    employee.streetAddress= "12 Broadway";
+    return employee
+  }  
+function deleteFromEmployeeByKey(employee, key){
+    var employees = {...employee};
+    delete employees.name;
+   return employees;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+}
+
+
+
